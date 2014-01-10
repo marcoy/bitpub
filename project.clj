bf779a3f1efd08f152eb5e629d55baf57f603735
Marco Yuen <marcoy@gmail.com>
2014-01-09 21:34:59 -0500

    Upgrade deps

--- project.clj
+++ project.clj
@@ -23,11 +23,11 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [cheshire "5.3.0"]
-                 [http-kit "2.1.15"]
+                 [cheshire "5.3.1"]
+                 [http-kit "2.1.16"]
                  [clj-http "0.7.8"]
-                 [clj-http-fake "0.4.1"]
+                 [clj-http-fake "0.7.8"]
                  [environ "0.4.0"]
-                 [com.novemberain/langohr "2.0.1"]]
+                 [com.novemberain/langohr "2.1.0"]]
 
   :plugins [[lein-environ "0.4.0"]])
